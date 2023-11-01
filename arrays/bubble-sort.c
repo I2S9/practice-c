44d28360e60f9a2c6adb8da494f2716f35f80a0a
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-11-01 23:11:46 +0100

    Update and rename tri-bulle.c to bubble-sort.c

--- arrays/bubble-sort.c
+++ arrays/bubble-sort.c
@@ -1,12 +1,10 @@
 typedef struct maillon_s maillon_t;
 typedef struct liste_s liste_t;
 
-
 struct maillon_s {
   element_t valeur;
   maillon_t * suivant;
 };
-
 
 struct liste_s {
   maillon_t * premier;
