adb0f90fa4bbd4f0ab7ac5396cd235809610f8a6
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-11-11 23:30:46 +0100

    Create Upper-&-Lower-Triangular-Matrix.c

---
+++ arrays/2D-dynamically-allocated-arrays/Upper-&-Lower-Triangular-Matrix.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+//Write 2 functions that should:
+//1. Create a "Lower triangular Matrix" and return it
+//2. Create an "Upper Triangular Matrix" and return it
+
+int** createLowerTriangular(int n)
+{
+    int** a;
+    int i;
+    a = (int**)malloc(n*sizeof(int*));
+    for(i=0;i<n;i++)
+    {
+        a[i] = (int*)malloc((i+1)*sizeof(int));
+    }
+    return a;
+}
