1c859150a55f1014250a0c2994573612ef8534b9
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-11-11 23:47:55 +0100

    Update Upper-&-Lower-Triangular-Matrix.c

--- arrays/2D-dynamically-allocated-arrays/Upper-&-Lower-Triangular-Matrix.c
+++ arrays/2D-dynamically-allocated-arrays/Upper-&-Lower-Triangular-Matrix.c
@@ -5,6 +5,37 @@
 //1. Create a "Lower triangular Matrix" and return it
 //2. Create an "Upper Triangular Matrix" and return it
 
+void printLowerTriangular(int** a, int n)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+    {
+        for(=j=0; j<n; j++) // Associated with a row
+        {
+            if(i<j)
+                printf("0");
+            else //i>=j
+                printf("%d", a[i][j]);
+        }
+    }
+    printf("\n");
+}
+
+void printUpperTriangular(int** a, int n)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+    {
+        for(=j=0; j<n; j++) // Associated with a row
+        {
+            if(i>j)
+                printf("0");
+            else //i<=j
+                printf("%d", a[i][j]);
+        }
+    }
+    printf("\n");
+}
 
 int** createUpperTriangular(int n)
 {
