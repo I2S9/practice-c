924ef87cc0e29c764fe892e592455e14ffbabaa9
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-11-20 23:33:27 +0100

    Update data-alignment.c

--- data-structures/data-alignment.c
+++ data-structures/data-alignment.c
@@ -13,7 +13,10 @@ void main()
 {
 	unsigned int totalStructBytes = sizeof(Point);
 	Point p1;
-
+	p1.x = 0xFF;
+	// Note: The information in "y" is not going to be that readable since it's stored in a "double" type.. (floating point, etc).
+	p1.y = 0x0123456789ABCDEF; // 8 bytes = 16 hexadecimal characters
+	p1.z = 0x1234ABCD; // 4 bytes = 8 hexadecimal characters
 	p1.x = 0xFF; // 1 byte of memory
 	p1.y = 0x1234; // 2 bytes of memory
 	p1.z = 0x6789ABCD; // 4 bytes of memory
