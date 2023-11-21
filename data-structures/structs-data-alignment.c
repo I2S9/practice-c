943b971d0b6752d648820c03698f457e124a382e
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-11-21 22:55:53 +0100

    Create structs-data-alignment.c

---
+++ data-structures/structs-data-alignment.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef struct point1 {
+	char x;
+	char z; 
+	double y;
+}Point1;
+
+typedef struct point {
+	char t; 
+	short x;
+	char z;
+	double y;
+}Point;
+
+void main()
+{
+	Point p1;
+	unsigned int totalBytes = sizeof(Point);
+	p1.t = 0x11;
+	p1.x = 0xAABB;
+	p1.z = 0xEE;
+	p1.y = 0x123456789ABCDEF;
+}
