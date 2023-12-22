557b14d84564686b44711127418e2e06e16ad68b
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-12-22 12:51:04 +0100

    Create data-alignment-padding.c

---
+++ structs-alignment/data-alignment-padding.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef struct point {
+	char x;
+	double y;
+	int z;
+}Point;
+
+void main()
+{
+	Point p1;
+	unsigned int totalBytes = sizeof(Point);
+
+	p1.x = 0xFF;
+	// Note: The information in "y" is not going to be that readable since it's stored in a "double" type.. (floating point, etc).
+	p1.y = 0x0123456789ABCDEF; // 8 bytes = 16 hexadecimal characters
+	p1.z = 0x1234ABCD; // 4 bytes = 8 hexadecimal characters
+}
